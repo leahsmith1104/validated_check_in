0636abc6b60c29515107c539a4fb6375526fec4c
leah smith <leahsmith1104@gmail.com>
2019-08-21 14:53:43 +0100

    update routes and pages

--- app/routes.js
+++ app/routes.js
@@ -18,17 +18,6 @@ router.post('/check-in/hasMeeting', function (req, res) {
   }
 })
 
-router.post('/check-in/has-meeting/surname', function (req, res) {
-
-  let selectedInitial = req.session.data['initial-last']
-
-  if (selectedInitial === 'W') {
-    res.redirect('/check-in/has-meeting/full-name')
-  } else {
-    res.redirect('/check-in/has-meeting/meeting-time')
-  }
-})
-
 router.post('/check-in/enquiryType', function (req, res) {
   // Get the answer from session data
   // The name between the quotes is the same as the 'name' attribute on the input elements
@@ -39,12 +28,6 @@ router.post('/check-in/enquiryType', function (req, res) {
   if (enquiryType === 'meeting') {
     res.redirect('/check-in/has-meeting/first-name')
   }
-  if (enquiryType === 'payments') {
-    res.redirect('/check-in/payments')
-  }
-  if (enquiryType === 'benefits') {
-    res.redirect('/check-in/benefits')
-  }
   if (enquiryType === 'facilities') {
     res.redirect('/check-in/facilities')
   }
@@ -53,50 +36,6 @@ router.post('/check-in/enquiryType', function (req, res) {
   }
   else {
     res.redirect('/check-in/no-meeting')
-  }
-})
-
-router.post('/check-in/has-meeting/coach-check', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  let coach = req.session.data['coach']
-
-  var uc = ["Farhana", "Charlotte","Paul","Sam","Bev"];
-  var heritage = ["Christine","Ian","Sonia","Belinda","Michael"];
-  if (uc.includes(coach)) {
-    res.redirect('check-in-uc')
-  }
-  if (heritage.includes(coach)) {
-    res.redirect('check-in-heritage')
-  }
-  if (coach === 'NES') {
-    res.redirect('check-in-group')
-  }
-  else {
-    res.redirect('check-in')
-  }
-})
-
-router.post('/check-in/enquiryType-v3', function (req, res) {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
-
-  let enquiryType = req.session.data['enquiry']
-
-  if (enquiryType === 'meeting') {
-    res.redirect('/check-in_v03/has-meeting/first-name')
-  }
-  if (enquiryType === 'facilities') {
-    res.redirect('/check-in_v03/facilities')
-  }
-  if (enquiryType === 'other') {
-    res.redirect('/check-in_v03/no-meeting')
-  }
-  else {
-    res.redirect('/check-in_v03/no-meeting')
   }
 })
 
